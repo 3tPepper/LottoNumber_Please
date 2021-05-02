b69e069e34a36d33a41a9bb50c299dd93df4013c
shortcutsB <shortcutsb@gmail.com>
2021-05-03 07:00:13 +0900

    배열 전역함수로 생성, 빈도수 배열에 값 넣을 함수 틀 생성

--- lottoPlease/main.c
+++ lottoPlease/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int numFrequency[45] = {0, };	// 931~961회차 당첨 빈도수
+int random[210] = {0, };		//빈도수 만큼 1~45 번호 입력 배열
 
 int main(void) {
+
+}
+
+void lFrequency() {				//random[210]에 빈도수만큼 번호 채우는 함수
+	int i, j, k = 0;
 
 }
